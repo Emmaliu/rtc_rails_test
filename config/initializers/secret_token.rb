40c91ce45567bd57699991de7f3ab666d432bb9f
Ronnie Taylor <ronnie@ronnietaylor.com>
2012-11-14 14:54:59 -0500

    initial import. bare bones rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ronnietaylor::Application.config.secret_token = '4c9e540b321009a840cfc8e700edc916bfbdf0e004ebd717197e361f1b353443ba631c4d66b039b6461dd55931507d6d86472f52c1441c38fbeca3261e0327ee'
